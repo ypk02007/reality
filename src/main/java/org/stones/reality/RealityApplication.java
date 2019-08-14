f02955edef349f826de2121addf3e0206408dd43
ethan <ifubabz@gmail.com>
2019-08-14 11:21:22 +0900

    Initial Maven Project

---
+++ src/main/java/org/stones/reality/RealityApplication.java
@@ -0,0 +1,20 @@
+package org.stones.reality;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * 클래스 설명: RealityApplication
+ *
+ * <pre>
+ * 
+ * </pre>
+ */
+public class RealityApplication {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(RealityApplication.class);
+	
+	public static void main(String[] args) {
+		LOGGER.debug("{}", "RealityApplication");
+	}
+}
---
+++ src/test/java/org/stones/reality/RealityApplicationTest.java
@@ -0,0 +1,13 @@
+package org.stones.reality;
+
+import static org.junit.Assert.assertSame;
+
+import org.junit.Test;
+
+public class RealityApplicationTest {
+
+	@Test
+	public void test() {
+		assertSame(true, true);
+	}
+}
